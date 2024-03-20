4ac61259e4e39f8b0652dc1e7c94533e03ff5747
BhavyaBh289 <shahbhavyasp@gmail.com>
2024-03-20 19:30:21 +0530

    final working

--- app.py
+++ app.py
@@ -4,22 +4,30 @@ import json
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def get_username():
-    return render_template('get_username.html')
+    if request.method == 'POST':
+        username = request.form.get('username')
+        users = get_users()
+        if username in users:
+            return redirect(url_for('view_points', username=username))
+        else:
+            return "User not found"
+    else:
+        return render_template('get_username.html')
 
 @app.route('/admin')
 def admin_index():
     users = get_users()
-    return render_template('index.html', users=users)
+    return render_template('admin_index.html', users=users)
 
 @app.route('/admin/give_points', methods=['POST'])
 def give_points():
     username = request.form.get('username')
-    points_attendence = int(request.form.get('points_attendence'))
+    points_attendance = int(request.form.get('points_attendance'))
     points_gatha = int(request.form.get('points_gatha'))
 
-    give_user_points(username, points_attendence, points_gatha)
+    give_user_points(username, points_attendance, points_gatha)
     return redirect(url_for('admin_index'))
 
 @app.route('/view_points/<username>')
@@ -39,17 +47,17 @@ def get_users():
     with open('users.json', 'r') as f:
         return json.load(f)
 
-def give_user_points(username, points_attendence, points_gatha):
+def give_user_points(username, points_attendance, points_gatha):
     users = get_users()
     if username in users:
-        users[username]['points_attendence'] += points_attendence
+        users[username]['points_attendance'] += points_attendance
         users[username]['points_gatha'] += points_gatha
-        users[username]['cumulative_points'] = users[username]['points_attendence'] + users[username]['points_gatha']
+        users[username]['cumulative_points'] = users[username]['points_attendance'] + users[username]['points_gatha']
     else:
         users[username] = {
-            'points_attendence': points_attendence,
+            'points_attendance': points_attendance,
             'points_gatha': points_gatha,
-            'cumulative_points': points_attendence + points_gatha
+            'cumulative_points': points_attendance + points_gatha
         }
 
     with open('users.json', 'w') as f:
